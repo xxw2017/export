89435ac38249a7901d34a59186d4f5cb5848deba
xxw <627926361@qq.com>
2018-03-06 14:36:50 +0800

    first commit

---
+++ src/main/java/cn/gzcb/export/ExportApplication.java
@@ -0,0 +1,12 @@
+package cn.gzcb.export;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExportApplication.class, args);
+	}
+}
