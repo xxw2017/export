c56c795722e6a65eff956a22d6c874bb1f14bc6a
misterZhou <1130271560@qq.com>
2018-03-06 17:07:22 +0800

    提交了几个测试用例

---
+++ src/test/java/cn/gzcb/export/blockingqueueTest/BlockingQueueTest.java
@@ -0,0 +1,41 @@
+package cn.gzcb.export.blockingqueueTest;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class BlockingQueueTest {
+
+    public static void main(String[] args) throws InterruptedException {
+        // 声明一个容量为10的缓存队列
+        BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10);
+
+        Producer producer1 = new Producer(queue);
+        Producer producer2 = new Producer(queue);
+        Producer producer3 = new Producer(queue);
+        Consumer consumer1 = new Consumer(queue);
+        Consumer consumer2 = new Consumer(queue);
+        Consumer consumer3 = new Consumer(queue);
+
+        // 借助Executors
+        ExecutorService service = Executors.newCachedThreadPool();
+        // 启动线程
+        service.execute(producer1);
+        service.execute(producer2);
+        service.execute(producer3);
+        service.execute(consumer1);
+        service.execute(consumer2);
+        service.execute(consumer3);
+
+        // 执行10s
+        Thread.sleep(10 * 1000);
+        producer1.stop();
+        producer2.stop();
+        producer3.stop();
+
+        Thread.sleep(2000);
+        // 退出Executor
+        service.shutdown();
+    }
+}
